8a3c5a24ba290f005b4d427f76156e84b8732450
Luis-edubr <moliveiraluiseduardo@gmail.com>
2026-05-01 10:34:36 -0300

    ajustes de arquitetura frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.home')
 
-        <title>{{ config('app.name', 'Imobiliária Hahn') }}</title>
-
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-
-        @vite(['resources/css/app.scss', 'resources/js/app.js'])
-    </head>
-    <body class="home-page">
-        <!-- HEADER -->
-        <x-site-header />
+@section('content')
 
         <!-- HERO & SEARCH -->
         <section class="hero">
@@ -173,8 +159,4 @@
             </div>
         </section>
 
-        <!-- FOOTER -->
-        <!-- FOOTER COMPONENT -->
-        <x-site-footer />
-    </body>
-</html>
+@endsection
